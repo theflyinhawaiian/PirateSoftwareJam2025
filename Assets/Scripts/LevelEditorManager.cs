f82aa6ac2ab05137c59596f9454cb7caba28fe4a
Peter Mullins <jc310xc@gmail.com>
2025-01-20 00:55:25 -0500

    feat: save/load rooms

--- Assets/Scripts/LevelEditorManager.cs
+++ Assets/Scripts/LevelEditorManager.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Assets.Model;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace Assets.Scripts {
     enum ViewMode {
@@ -13,25 +13,21 @@ namespace Assets.Scripts {
     }
 
     public class LevelEditorManager : MonoBehaviour {
-        public GameObject panel;
-        public TMP_Text viewModeText;
         public TMP_Dropdown obstacleSelector;
-        public TMP_InputField xInput;
-        public TMP_InputField yInput;
-        public TMP_InputField zInput;
         public TMP_Dropdown roomsDropdown;
         public TMP_InputField saveRoomInput;
         private readonly string obstaclesPath = "Assets/Prefabs/Obstacles";
         private readonly string roomsPath = "Assets/Resources/Rooms";
         private List<GameObject> obstacleGameObjects = new();
         private GameObject selectedObject;
-        float screenWidth;
-        RectTransform panelTransform;
+        private List<GameObject> roomContents = new();
+        private int nextId = 0;
+
+        private RoomFileHandler roomSerializer;
 
         void Start()
-        { 
-            panelTransform = panel.GetComponent<RectTransform>();
-
+        {
+            roomSerializer = new RoomFileHandler();
             PopulateObstacles();
 
             PopulateRooms();
@@ -39,23 +35,10 @@ namespace Assets.Scripts {
 
         void Update()
         {
-            if(screenWidth != Screen.width)
-                AdjustUISize();
-
-            if (Input.GetMouseButtonDown(0))
-                HandleMouseClick();                            
-
-            if (selectedObject != null)
-                UpdateCoordinateText();
-        }
-
-        private void AdjustUISize()
-        {
-            
-            screenWidth = Screen.width;
-            var offsetMin = panelTransform.offsetMin;
-            offsetMin.x = Screen.width * .8f;
-            panelTransform.offsetMin = offsetMin;
+            if(Selection.activeGameObject != selectedObject){
+                selectedObject = Selection.activeGameObject;
+                Debug.Log(selectedObject);
+            }
         }
 
         private void PopulateObstacles()
@@ -95,9 +78,11 @@ namespace Assets.Scripts {
 
             if (selectedObstacle != null)
             {
-                Vector3 position = new(50, 50, 0);
-                Instantiate(selectedObstacle, position, Quaternion.identity);
+                Vector3 position = new(0, 0, 0);
+                var obj = Instantiate(selectedObstacle, position, Quaternion.identity);
+                InitializeEntity(obj);
                 Debug.Log($"Instantiated: {selectedObstacle.name}");
+                roomContents.Add(obj);
             }
             else
             {
@@ -105,103 +90,78 @@ namespace Assets.Scripts {
             }
         }
 
+        private void InitializeEntity(GameObject entity){
+            var behavior = entity.GetComponent<EntityBehavior>();
+            // TODO: Give the behavior a place to read moveSpeed value from so we can control it more dynamically
+            behavior.moveSpeed = 0;
+            behavior.id = nextId;
+
+            // increment nextId so the next entity initialized has a unique Id
+            nextId++;
+        }
+
         public void DeleteObject()
         {
-            if (selectedObject != null)
-            {
-                Debug.Log($"Deleting Object: {selectedObject.name}");
-
-                Destroy(selectedObject);
-
-                selectedObject = null;
-
-                xInput.text = "";
-                yInput.text = "";
-                zInput.text = "";
-            }
-            else
+            if (selectedObject == null)
             {
                 Debug.LogWarning("No object selected to delete.");
+                return;
             }
-        }
 
-        public void ToggleViewMode() 
-        {
+            Debug.Log($"Deleting Object: {selectedObject.name}");
 
-        }
+            roomContents.Remove(selectedObject);
 
-        #region Object-Movement
-
-        private void HandleMouseClick()
-        {
-            if (EventSystem.current.IsPointerOverGameObject()) { return; }
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                GameObject clickedObject = hit.collider.gameObject;
-
-                if (clickedObject != selectedObject)
-                {
-                    SelectObject(clickedObject);
-                }
-            }
-            else
-            {
-                DeselectObject();
-            }
-        }
-
-        void SelectObject(GameObject obj)
-        {
-            selectedObject = obj;
-
-            Debug.Log($"Selected Object: {selectedObject.name}");
-
-            UpdateCoordinateText();
-        }
-
-        private void DeselectObject()
-        {
-            if (selectedObject != null)
-            {
-                Debug.Log($"Deselected Object: {selectedObject.name}");
-            }
+            Destroy(selectedObject);
 
             selectedObject = null;
-
-            xInput.text = "";
-            yInput.text = "";
-            zInput.text = "";
         }
-
-        public void UpdateCoordinateText()
-        {
-            if (selectedObject == null) return;
-
-            Vector3 currentPos = selectedObject.transform.position;
-
-            xInput.text = currentPos.x.ToString("F2");
-            yInput.text = currentPos.y.ToString("F2");
-            zInput.text = currentPos.z.ToString("F2");
-        }
-
-        #endregion
 
         public void ListItemSelected()
         {
         }
 
-
         public void LoadRoom()
         {
+            var room = roomSerializer.LoadRoom("myRoom");
 
+            foreach(var entity in room.Entities){
+                var gameObj = obstacleGameObjects[(int)entity.Type];
+                var instance = Instantiate(gameObj);
+                var transform = instance.transform;
+                transform.position = new Vector3(entity.XPosition, entity.YPosition, entity.ZPosition);
+                transform.rotation = new Quaternion(entity.XRotation, entity.YRotation, entity.ZRotation, 1);
+                transform.localScale = new Vector3(entity.XScale, entity.YScale, entity.ZScale);
+                var meta = instance.GetComponent<EntityBehavior>();
+
+                meta.moveSpeed = 0;
+                meta.id = entity.Id;
+            }
         }
 
         public void SaveRoom()
         {
-
+            var room = new Room {
+                Entities = roomContents.Select(x => {
+                    var meta = x.GetComponent<EntityBehavior>();
+                    var objTransform = x.transform;
+                    return new Entity {
+                            // Need to strip the '(Clone)' portion of the name
+                            Type = Entity.ToEntityType(x.name.Substring(0, x.name.IndexOf('('))),
+                            XPosition = objTransform.position.x,
+                            YPosition = objTransform.position.y,
+                            ZPosition = objTransform.position.z,
+                            XRotation = objTransform.rotation.x,
+                            YRotation = objTransform.rotation.y,
+                            ZRotation = objTransform.rotation.z,
+                            XScale = objTransform.localScale.x,
+                            YScale = objTransform.localScale.y,
+                            ZScale = objTransform.localScale.z,
+                            Id = meta.id
+                        };
+                    }).ToList()
+                };
+            roomSerializer.SaveRoom(room, "myRoom");
         }
     }
 }
