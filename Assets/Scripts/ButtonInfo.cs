3c58571b52f7d2500f862035911646e46bfcb3d6
Mike McGhen <mcghenmike@gmail.com>
2025-01-21 20:01:12 -0500

    feat: Shop refactoring

--- Assets/Scripts/ButtonInfo.cs
+++ Assets/Scripts/ButtonInfo.cs
@@ -1,18 +1,23 @@
-using System.Security.Cryptography.X509Certificates;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ButtonInfo : MonoBehaviour
 {
     public int ItemID;
     public TMP_Text PriceTxt;
     public TMP_Text QuantityTxt;
-    public GameObject ShopManager;
+    public ShopManager ShopManager;
 
     void Update()
     {
-        PriceTxt.text = "Price: " + ShopManager.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
-        QuantityTxt.text = ShopManager.GetComponent<ShopManager>().shopItems[3, ItemID].ToString();
+        if (ShopManager != null && ShopManager.shopItems.TryGetValue(ItemID, out var item))
+        {
+            PriceTxt.text = "Price: " + item.Price.ToString();
+            QuantityTxt.text = "Quantity: " + item.Quantity.ToString();
+        }
+        else
+        {
+            Debug.LogWarning($"Item with ID {ItemID} not found or ShopManager is not assigned.");
+        }
     }
 }
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,46 +1,47 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    public int[,] shopItems = new int[5, 5];
     public float money;
     public TMP_Text MoneyTXT;
+
+    public Dictionary<int, Item> shopItems = new();
 
     void Start()
     {
         MoneyTXT.text = "Money: " + money.ToString();
 
-        //ID's
-        shopItems[1, 1] = 1;
-        shopItems[1, 2] = 2;
-        shopItems[1, 3] = 3;
-        shopItems[1, 4] = 4;
-        
-        //Price
-        shopItems[2, 1] = 10;
-        shopItems[2, 2] = 20;
-        shopItems[2, 3] = 30;
-        shopItems[2, 4] = 40;
-
-        //Quantity
-        shopItems[3, 1] = 0;
-        shopItems[3, 2] = 0;
-        shopItems[3, 3] = 0;
-        shopItems[3, 4] = 0;
+        shopItems[1] = new () { Id = 1, Price = 10, Quantity = 0 };
+        shopItems[2] = new () { Id = 2, Price = 20, Quantity = 0 };
+        shopItems[3] = new () { Id = 3, Price = 30, Quantity = 0 };
+        shopItems[4] = new () { Id = 4, Price = 40, Quantity = 0 };
     }
 
     public void Buy()
     {
-        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
-        if (money >= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        GameObject buttonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+        ButtonInfo buttonInfo = buttonRef.GetComponent<ButtonInfo>();
+        int itemId = buttonInfo.ItemID;
+
+        if (shopItems.TryGetValue(itemId, out Item item))
         {
-            money -= shopItems[2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
-            shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID]++;
-            MoneyTXT.text = "Money: " + money.ToString();
-            ButtonRef.GetComponent<ButtonInfo>().QuantityTxt.text = shopItems[3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
+            if (money >= item.Price)
+            {
+                money -= item.Price;
+                item.Quantity++;
+                MoneyTXT.text = $"Money: {money.ToString()}";
+                buttonInfo.QuantityTxt.text = item.Quantity.ToString();
+            }
         }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
