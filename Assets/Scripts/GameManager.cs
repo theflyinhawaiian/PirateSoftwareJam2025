ba563de6d4369f2c4396b201f92ec6c134be7e34
Peter Mullins <jc310xc@gmail.com>
2025-01-23 20:41:36 -0500

    fix: decouple text UI from game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public TMP_Text moneyText;
-    public TMP_Text healthText;
-
     public int playerMoney = 0;
     public int playerHealth = 3;
     public ISpawner spawner;
@@ -17,9 +14,6 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         spawner = new MixedSpawner(transform, this);
-
-        moneyText.text = $"Player Money: {playerMoney}";
-        healthText.text = $"Player Health: {playerHealth}";
     }
 
     void Update()
@@ -30,13 +24,11 @@ public class GameManager : MonoBehaviour
     public void TargetDestroyed(int value){
         playerMoney += value;
         Debug.Log($"Money: {playerMoney}");
-        moneyText.text = $"Player Money: {playerMoney}";
     }
 
     public void ObstacleImpacted(int damageValue){
         playerHealth -= damageValue;
         Debug.Log($"Health: {playerHealth}");
-        healthText.text = $"Player Health: {playerHealth}";
 
         if(playerHealth <= 0){
             // fuckin die
