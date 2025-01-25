e31a958d8f3df229f1922f6cccdf0fa61d62141e
Peter Mullins <jc310xc@gmail.com>
2025-01-25 15:34:26 -0500

    feat: follow behavior, player/spawn bounds

--- Assets/Scripts/EntityBehavior.cs
+++ Assets/Scripts/EntityBehavior.cs
@@ -5,7 +5,7 @@ public class EntityBehavior : MonoBehaviour
     public float moveSpeed = 5;
     public int id;
 
-    Rigidbody rb;
+    protected Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -14,6 +14,19 @@ public class EntityBehavior : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(new Vector3(rb.position.x, rb.position.y, rb.position.z - (moveSpeed * Time.deltaTime)));
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        var r = GetComponent<Renderer>();
+        if (r == null)
+            return;
+        var bounds = r.bounds;
+        Gizmos.matrix = Matrix4x4.identity;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(bounds.center, bounds.extents * 2);
+
+        Gizmos.DrawRay(transform.position, Vector3.back * 10000);
     }
 }
 
--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -10,8 +10,19 @@ public class TargetSpawner : ISpawner
 
     float lastSpawnTime = 0;
 
+    public float targetX;
+    public float targetY;
+
+    float extentX;
+    float extentY;
+
+    float leftBounds;
+    float rightBounds;
+    float topBounds;
+    float bottomBounds;
+
     Transform origin;
-    public TargetSpawner(Transform origin, GameManager manager)
+    public TargetSpawner(Transform origin, Transform bounds, GameManager manager)
     {
         this.origin = origin;
         gameManager = manager;
@@ -26,6 +37,24 @@ public class TargetSpawner : ISpawner
             return;
 
         this.targetPrefab = targetPrefab;
+
+        leftBounds = bounds.Find("left").position.x;
+        rightBounds = bounds.Find("right").position.x;
+        topBounds = bounds.Find("top").position.y;
+        bottomBounds = bounds.Find("bottom").position.y;
+
+        // there's seemingly no way to do this from a prefab, so we'll spawn 
+        // in a sample boi and get its info, destroying it when we're done
+        var sample = Object.Instantiate(targetPrefab, new Vector3(1000, 1000, 1000), Quaternion.identity);
+        var extents = sample.GetComponent<Renderer>().bounds.extents;
+        extentX = extents.x;
+        extentY = extents.y;
+        Object.Destroy(sample);
+    }
+
+    public void SetSpawnCoords(float x, float y){
+        targetX = x;
+        targetY = y;
     }
 
     public void Spawn()
@@ -34,10 +63,10 @@ public class TargetSpawner : ISpawner
         if(lastSpawnTime + spawnInterval > currTime)
             return;
 
-        var targetX = Random.value * 10;
-        var targetY = Random.value * 10;
-        var targetPos = new Vector3(targetX, targetY, origin.position.z);
-        var obj = GameObject.Instantiate(targetPrefab);
+        var spawnX = leftBounds + extentX + (Random.value * (rightBounds - extentX));
+        var spawnY = bottomBounds + extentY + (Random.value * (topBounds - extentY));
+        var targetPos = new Vector3(spawnX, spawnY, origin.position.z);
+        var obj = Object.Instantiate(targetPrefab);
         obj.transform.position = targetPos;
         var behavior = obj.GetComponent<TargetBehavior>();
         behavior.SetManager(gameManager);
