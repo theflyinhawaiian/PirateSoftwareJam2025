5652d05b077a17026721988eb9a210c6088a4f44
Mike McGhen <mcghenmike@gmail.com>
2025-01-26 14:09:05 -0500

    feat: Adds PowerUp folder and IUpgrade

---
+++ Assets/Model/PowerUps/IUpgrade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Model.PowerUps
+{
+    public interface IUpgrade
+    {
+        public int Cost {  get; set; }
+        public int Quantity { get; set; }
+        public string UpgradeName { get; set; }
+    }
+}
